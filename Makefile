5f32879bb0daa9a71ce97f0cbf6c20b4113d1661
Christian Marcellino <cammar12177@gmail.com>
2025-04-07 15:22:45 -0500

    Fixed TESTS.hpp and also added a file named test1

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@ ARGS = -g -Wall -std=c++11
 
 main: cas-lib server client
 
+test1: tests/test1.cpp 
+	g++ $(ARGS) $< cas/http-server.o cas/string-helpers.o cas/http-server-context.o cas/http-client.o -o test1
+	./test1
+
 server: httpServer.cpp
 	g++ $(ARGS) $< cas/http-server.o cas/string-helpers.o cas/http-server-context.o -o server
 
@@ -12,4 +16,4 @@ cas-lib:
 	make -C ./cas -f makelib.mk
 
 clean:
-	rm server client *.o | make -C ./cas -f makelib.mk clean
+	rm server client test1 *.o | make -C ./cas -f makelib.mk clean
---
+++ tests/test1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "TESTS.hpp"
+
+#include "../cas/libs/http-client.hpp"
+#include "../cas/libs/http-server.hpp"
+
+TESTS()
+{
+    cas::HttpServer server(60001, 1024);
+    auto contextAsync = server.get_ctx_async();
+
+    cas::HttpClient client("127.0.0.1", 60001, 4096);
+    cas::HttpClientRequest request;
+    request.path = "/";
+    auto clientResponseAsync = client.get_async(request);
+
+    auto context = contextAsync.get();
+
+    std::cout << "Client Request: " << std::endl;
+    std::cout << context.request.to_string() << std::endl;
+
+    context.response.protocol = "HTTP/1.1";
+    context.response.statusCode = 200;
+    context.response.statusMessage = "Success";
+    context.response.body = "This worked!";
+    context.response.send_and_close_async().get();
+
+    auto clientResponse = clientResponseAsync.get();
+
+    std::cout << "Client Response: " << std::endl;
+    std::cout << clientResponse.to_string() << std::endl;
+}
