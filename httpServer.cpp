54a424c61196fcbf3de918c33ade18efb990406a
Ryan <rdw40@students.uwf.edu>
2025-04-11 15:15:30 -0500

    added login and signup

--- httpServer.cpp
+++ httpServer.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <fstream>
+#include <vector>
 #include "cas/libs/http-server.hpp"
 #include "libs/helpers.hpp"
 #include "cas/libs/string-helpers.hpp"
@@ -12,6 +13,43 @@ struct UserInfo
 
 cas::HttpServer g_Server(60001, DEFAULT_SERVER_BUFFER_SIZE);
 std::map<std::string, UserInfo> _loggedInUsers;
+std::vector<UserInfo> g_Users;
+
+void save_file()
+{
+    using namespace std;
+    ofstream oss("users.txt");
+
+    for (auto user : g_Users)
+    {
+        oss << user.username << "," << user.password << endl;
+    }
+    oss.close();
+}
+
+void load_file()
+{
+    using namespace std;
+    g_Users.clear();
+    ifstream iss("users.txt");
+
+    if (iss.is_open())
+    {
+        string line;
+        while (getline(iss, line))
+        {
+            auto data = strhelp::split(line, ',');
+            if(data.size() <= 1){
+                continue;
+            }
+            UserInfo userinfo;
+            userinfo.username = data[0];
+            userinfo.password = data[1];
+            g_Users.push_back(userinfo);
+        }
+        iss.close();
+    }
+}
 
 void handleContext(cas::HttpServerContext& ctx)
 {
@@ -38,12 +76,19 @@ void handleContext(cas::HttpServerContext& ctx)
             return;
         }
 
+        for(auto user : g_Users){
+            if(user.username == userInfo.username){
+            ctx.response.set_status(cas::HttpResponse::Status::BadRequest);
+            ctx.response.body = "Username already exists";
+            ctx.response.sendoff_close_async(g_Server).get();
+            return;
+            }
+
+        }
         auto token = generate_token(base64_encode(userInfo.username.substr(0, 5)));
-
-        // TODO: check if the user already exists
-
-        _loggedInUsers[token] = userInfo;
-
+         _loggedInUsers[token] = userInfo;
+        g_Users.push_back(userInfo);
+        save_file();
         ctx.response.headers["Authorization"] = "Bearer " + token;
         ctx.response.sendoff_close_async(g_Server).get();
 
@@ -76,20 +121,29 @@ void handleContext(cas::HttpServerContext& ctx)
             return;
         }
 
-        auto token = generate_token(base64_encode(userInfo.username.substr(0, 5)));
+        
 
-        // TODO: check if the user already exists
+        for(auto user : g_Users){
+            if(user.username == userInfo.username && user.password == userInfo.password){
+                auto token = generate_token(base64_encode(userInfo.username.substr(0, 5)));
+            _loggedInUsers[token] = userInfo;
 
-        _loggedInUsers[token] = userInfo;
+            ctx.response.headers["Authorization"] = "Bearer " + token;
+            ctx.response.sendoff_close_async(g_Server).get();
+    
+            std::cout << "Logged in a user." << std::endl;
+            std::cout << "Username: " << userInfo.username << std::endl;
+            std::cout << "Password: " << userInfo.password << std::endl;
+            std::cout << "Bearer Token: " << token << std::endl;
+            return;
+            }
 
-        ctx.response.headers["Authorization"] = "Bearer " + token;
-        ctx.response.sendoff_close_async(g_Server).get();
+        }
 
-        std::cout << "Logged in a user." << std::endl;
-        std::cout << "Username: " << userInfo.username << std::endl;
-        std::cout << "Password: " << userInfo.password << std::endl;
-        std::cout << "Bearer Token: " << token << std::endl;
-        return;
+        ctx.response.set_status(cas::HttpResponse::Status::BadRequest);
+            ctx.response.body = "Credentials are INVALID";
+            ctx.response.sendoff_close_async(g_Server).get();
+            return;
     }
     // ===========
     // | LOG OUT |
@@ -182,6 +236,11 @@ void handleContext(cas::HttpServerContext& ctx)
 
 int main(int argc, char** argv)
 {
+    load_file();
+
+
+
+
     g_Server.OnCloseClientConnection = [](int clientId) {
         // handle a closing client connection
         std::cout << "Connection was closed: " << clientId << std::endl;
