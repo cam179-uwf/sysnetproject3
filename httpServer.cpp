ff858401fe20b975152540678ef914c1316455ce
Christian Marcellino <cam179@students.uwf.edu>
2025-04-11 16:04:22 -0500

    Added more backend stuff

--- httpServer.cpp
+++ httpServer.cpp
@@ -12,7 +12,7 @@ struct UserInfo
 };
 
 cas::HttpServer g_Server(60001, DEFAULT_SERVER_BUFFER_SIZE);
-std::map<std::string, UserInfo> _loggedInUsers;
+std::map<std::string, UserInfo> g_Sessions;
 std::vector<UserInfo> g_Users;
 
 void save_file()
@@ -54,6 +54,26 @@ void load_file()
 
 void handleContext(cas::HttpServerContext &ctx)
 {
+    std::string bearer;
+    std::string auth;
+    bool isAuthorized = false;
+
+    if (ctx.request.try_get_header("Authorization", auth))
+    {
+        auto data = strhelp::split(auth, ' ', 2);
+
+        if (data.size() > 1)
+        {
+            bearer = data[1];
+            std::cout << "Client sent their bearer token: " << bearer << std::endl;
+
+            if (g_Sessions.find(bearer) != g_Sessions.end())
+            {
+                isAuthorized = true;
+            }   
+        }
+    }
+
     // ===========
     // | SIGN UP |
     // ===========
@@ -87,10 +107,13 @@ void handleContext(cas::HttpServerContext &ctx)
                 return;
             }
         }
+
         auto token = generate_token(base64_encode(userInfo.username.substr(0, 5)));
-        _loggedInUsers[token] = userInfo;
+
+        g_Sessions[token] = userInfo;
         g_Users.push_back(userInfo);
         save_file();
+        
         ctx.response.headers["Authorization"] = "Bearer " + token;
         ctx.response.sendoff_close_async(g_Server).get();
 
@@ -128,7 +151,7 @@ void handleContext(cas::HttpServerContext &ctx)
             if (user.username == userInfo.username && user.password == userInfo.password)
             {
                 auto token = generate_token(base64_encode(userInfo.username.substr(0, 5)));
-                _loggedInUsers[token] = userInfo;
+                g_Sessions[token] = userInfo;
 
                 ctx.response.headers["Authorization"] = "Bearer " + token;
                 ctx.response.sendoff_close_async(g_Server).get();
@@ -151,9 +174,17 @@ void handleContext(cas::HttpServerContext &ctx)
     // ===========
     else if (ctx.request.get_method() == "POST" && ctx.request.get_path() == "/logout")
     {
+        if (!isAuthorized)
+        {
+            ctx.response.set_status(cas::HttpResponse::Status::Unauthorized);
+            ctx.response.body = "You must be logged in to log out you idiot.";
+            ctx.response.sendoff_close_async(g_Server).get();
+            return;
+        }
+
         std::string bearer;
 
-        if (!ctx.request.try_get_header("Authorization", bearer) || is_whitespace(ctx.request.get_headers()["Authorization"]))
+        if (!ctx.request.try_get_header("Authorization", bearer) || is_whitespace(bearer))
         {
             ctx.response.set_status(cas::HttpResponse::Status::BadRequest);
             ctx.response.body = "You must include a bearer when logging out.";
@@ -161,11 +192,11 @@ void handleContext(cas::HttpServerContext &ctx)
             return;
         }
 
-        auto result = strhelp::split(ctx.request.get_headers()["Authorization"], ' ', 2);
+        auto data = strhelp::split(bearer, ' ', 2);
 
-        if (result.size() > 1)
+        if (data.size() > 1)
         {
-            bearer = result[1];
+            bearer = data[1];
         }
         else
         {
@@ -175,14 +206,14 @@ void handleContext(cas::HttpServerContext &ctx)
             return;
         }
 
-        if (_loggedInUsers.find(bearer) != _loggedInUsers.end())
+        if (g_Sessions.find(bearer) != g_Sessions.end())
         {
-            auto user = _loggedInUsers[bearer];
+            auto user = g_Sessions[bearer];
             std::cout << "Logged out a user." << std::endl;
             std::cout << "Username: " << user.username << std::endl;
             std::cout << "Password: " << user.password << std::endl;
             std::cout << "Bearer Token: " << bearer << std::endl;
-            _loggedInUsers.erase(bearer);
+            g_Sessions.erase(bearer);
         }
 
         ctx.response.body = "Logged you out.";
@@ -191,47 +222,69 @@ void handleContext(cas::HttpServerContext &ctx)
         g_Server.close_client_connection(ctx.get_client_fd()); // close the connection
         return;
     }
-    else
+    // ==================
+    // | Am I Logged In |
+    // ==================
+    else if (ctx.request.get_method() == "GET" && ctx.request.get_path() == "/amiloggedin")
     {
-        std::string bearer;
-        bool isAuthorized = false;
-
-        if (ctx.request.get_headers().find("Authorization") != ctx.request.get_headers().end())
-        {
-            auto result = strhelp::split(ctx.request.get_headers()["Authorization"], ' ', 2);
-
-            if (result.size() > 1)
-            {
-                bearer = result[1];
-                std::cout << "Bearer: " << bearer << std::endl;
-
-                if (_loggedInUsers.find(bearer) != _loggedInUsers.end())
-                {
-                    isAuthorized = true;
-                }
-                else
-                {
-                    ctx.response.set_status(cas::HttpResponse::Status::Unauthorized);
-                    ctx.response.sendoff_close_async(g_Server).get();
-                    return;
-                }
-            }
-            else
-            {
-                ctx.response.set_status(cas::HttpResponse::Status::Unauthorized);
-                ctx.response.sendoff_close_async(g_Server).get();
-                return;
-            }
-        }
-
         if (isAuthorized)
         {
-            ctx.response.body = "You are logged in.";
+            ctx.response.set_status(cas::HttpResponse::Status::OK);
+            ctx.response.sendoff_close_async(g_Server).get();
+        }
+        else
+        {
+            ctx.response.set_status(cas::HttpResponse::Status::Unauthorized);
+            ctx.response.sendoff_close_async(g_Server).get();
+        }
+    }
+    // ===================
+    // | Change Password |
+    // ===================
+    else if (ctx.request.get_method() == "POST" && ctx.request.get_path() == "/changepassword")
+    {
+        if (!isAuthorized)
+        {
+            ctx.response.set_status(cas::HttpResponse::Status::Unauthorized);
+            ctx.response.sendoff_close_async(g_Server).get();
+            return;
+        }
+        
+        std::string oldPassword;
+        std::string newPassword;
+
+        if (!ctx.request.try_get_header("oldpassword", oldPassword) || is_whitespace(oldPassword))
+        {
+            ctx.response.set_status(cas::HttpResponse::Status::BadRequest);
+            ctx.response.body = "You must include the oldpassword header when changing your password.";
             ctx.response.sendoff_close_async(g_Server).get();
             return;
         }
 
+        if (!ctx.request.try_get_header("newpassword", newPassword) || is_whitespace(newPassword))
+        {
+            ctx.response.set_status(cas::HttpResponse::Status::BadRequest);
+            ctx.response.body = "You must include the newpassword header when changing your password.";
+            ctx.response.sendoff_close_async(g_Server).get();
+            return;
+        }
+
+        if (g_Sessions[bearer].password == oldPassword)
+        {
+            g_Sessions[bearer].password = newPassword;
+
+            ctx.response.set_status(cas::HttpResponse::Status::OK);
+            ctx.response.body = "Password changed successfully.";
+            ctx.response.sendoff_close_async(g_Server).get();
+
+            save_file();
+            return;
+        }
+        
+        ctx.response.set_status(cas::HttpResponse::Status::BadRequest);
+        ctx.response.body = "Old password did not match.";
         ctx.response.sendoff_close_async(g_Server).get();
+        return;
     }
 }
 
